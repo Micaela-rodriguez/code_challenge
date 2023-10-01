79894a04f800ec7d9f5c16d71a069c677be4b35c
Micaela-rodriguez <micaela.roho@outlook.com>
2023-10-01 13:30:59 -0300

    cambios

--- logica.js
+++ logica.js
@@ -1,16 +1,16 @@
-let url='jsonplaceholder.typicode.com/users';
 let boton=document.getElementById("btn");
+
+boton.addEventListener('click', () => {
+    let url='https://jsonplaceholder.typicode.com/users';
+
 
 let nombre=document.getElementById("name");
 let apellido=document.getElementById("ape");
 let date=document.getElementById("fecha");
-
-boton.addEventListener('click', () => {
-
 fetch(url, {
 headers: { 'Content-Type': 'application/json; charset=utf-8' },
 method: 'POST',
-body: JSON.stringify({
+body:JSON.stringify({
     name:nombre.value,
     apell:apellido.value,
     nacimineto:date.value,
@@ -29,3 +29,4 @@ body: JSON.stringify({
 
 
 });
+
